9f3017e776798727f889ccf7606a362261ebd897
MisterS <sedj.13@hotmail.com>
2025-03-27 14:27:14 +0300

    Update Urok_8.z.1.py

--- Urok_8.z.1/Urok_8.z.1.py
+++ Urok_8.z.1/Urok_8.z.1.py
@@ -1,15 +1,14 @@
-# Ввод числа N
+
 N = int(input())
 
-# Ввод N чисел и сохранение их в список
+
 numbers = []
 for _ in range(N):
     numbers.append(int(input()))
 
-# Перевертывание списка
+
 reversed_numbers = numbers[::-1]
 
-# Вывод перевернутого списка
 for number in reversed_numbers:
     print(number)
 
